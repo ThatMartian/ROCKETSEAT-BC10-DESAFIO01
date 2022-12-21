d2663f4fcc03795cbe962c7277dd5113300ada2e
ThatMartian <98262778+ThatMartian@users.noreply.github.com>
2022-12-21 16:27:32 +0000

    White space only

--- assets/vendor/venobox/venobox.js
+++ assets/vendor/venobox/venobox.js
@@ -340,3 +340,476 @@
                 }); // click
 
                 /* -------- CHECK NEXT / PREV -------- */
+                function checknav(){
+
+                    thisgall = obj.data('gall');
+                    numeratio = obj.data('numeratio');
+                    gallItems = obj.data('gallItems');
+                    infinigall = obj.data('infinigall');
+                    share = obj.data('share');
+                    blockshare.html('');
+                    if ( obj.data('vbtype') !== 'iframe' && obj.data('vbtype') !== 'inline' && obj.data('vbtype') !== 'ajax' ) {
+                        sharelinks = { 
+                            pinterest : '<a target="_blank" href="https://pinterest.com/pin/create/button/?url='+obj.prop('href')+'&media='+obj.prop('href')+'&description='+title+'">'+pinIcon+'</a>', 
+                            facebook  : '<a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u='+obj.prop('href')+'">'+fbIcon+'</a>', 
+                            twitter   : '<a target="_blank" href="https://twitter.com/intent/tweet?text='+title+'&url='+obj.prop('href')+'">'+twitterIcon+'</a>', 
+                            linkedin  : '<a target="_blank" href="https://www.linkedin.com/sharing/share-offsite/?url='+obj.prop('href')+'">'+linkedinIcon+'</a>',
+                            download  : '<a target="_blank" href="'+obj.prop('href')+'">'+downloadIcon+'</a>'
+                        };
+                        $.each( share, function( key, value ) {
+                            blockshare.append(sharelinks[value]);
+                        });
+                    }
+
+                    if (gallItems) {
+                        items = gallItems;
+                    } else {
+                        items = $('.vbox-item[data-gall="' + thisgall + '"]');
+                    }
+
+                    if (items.length < 2) {
+                        infinigall = false;
+                        numeratio = false;
+                    }
+
+                    thenext = items.eq( items.index(obj) + 1 );
+                    theprev = items.eq( items.index(obj) - 1 );
+
+                    if (!thenext.length && infinigall === true) {
+                      thenext = items.eq(0);
+                    }
+
+                    // update gall numeration
+                    if (items.length >= 1) {
+                      gallIndex = items.index(obj)+1;
+                      blocknum.html(gallIndex + ' / ' + items.length);
+                    } else {
+                      gallIndex = 1;
+                    }
+                    if (numeratio === true) {
+                      blocknum.show();
+                    } else {
+                      blocknum.hide();
+                    }
+
+                    // update title
+                    if (title !== '') {
+                      blocktitle.show();
+                    } else {
+                      blocktitle.hide();
+                    }
+
+                    // update navigation arrows
+                    if (!thenext.length && infinigall !== true) {
+                      $('.vbox-next').css('display', 'none');
+                      nextok = false;
+                    } else {
+                      $('.vbox-next').css('display', 'block');
+                      nextok = true;
+                    }
+
+                    if (items.index(obj) > 0 || infinigall === true) {
+                      $('.vbox-prev').css('display', 'block');
+                      prevok = true;
+                    } else {
+                      $('.vbox-prev').css('display', 'none');
+                      prevok = false;
+                    }
+                    // activate swipe
+                    if (prevok === true || nextok === true) {
+                      content.on(TouchMouseEvent.DOWN, onDownEvent);
+                      content.on(TouchMouseEvent.MOVE, onMoveEvent);
+                      content.on(TouchMouseEvent.UP, onUpEvent);
+                    }
+                }
+
+                /* -------- gallery navigation -------- */
+                function navigateGall(destination) {
+
+                    if (destination.length < 1) {
+                      return false;
+                    }
+                    if (keyNavigationDisabled) {
+                      return false;
+                    }
+                    keyNavigationDisabled = true;
+
+                    overlayColor = destination.data('overlay') || destination.data('overlaycolor');
+
+                    framewidth = destination.data('framewidth');
+                    frameheight = destination.data('frameheight');
+                    border = destination.data('border');
+                    bgcolor = destination.data('bgcolor');
+                    dest = destination.data('href') || destination.attr('href');
+
+                    autoplay = destination.data('autoplay');
+
+                    title = (destination.data('titleattr') && destination.attr(destination.data('titleattr'))) || '';
+
+                    // swipe out item
+                    if (destination === theprev) {
+                      content.addClass('vbox-animated').addClass('swipe-right');
+                    }
+                    if (destination === thenext) {
+                      content.addClass('vbox-animated').addClass('swipe-left');
+                    }
+
+                    $preloader.show();
+
+                    content.animate({
+                      opacity : 0,
+                    }, 500, function(){
+
+                        overlay.css('background',overlayColor);
+
+                        content
+                        .removeClass('vbox-animated')
+                        .removeClass('swipe-left')
+                        .removeClass('swipe-right')
+                        .css({'margin-left': 0,'margin-right': 0});
+
+                        if (destination.data('vbtype') == 'iframe') {
+                            loadIframe();
+                        } else if (destination.data('vbtype') == 'inline') {
+                            loadInline();
+                        } else if (destination.data('vbtype') == 'ajax') {
+                            loadAjax();
+                        } else if (destination.data('vbtype') == 'video') {
+                            loadVid(autoplay);
+                        } else {
+                            content.html('<img src="'+dest+'">');
+                            preloadFirst();
+                        }
+                        obj = destination;
+                        checknav();
+                        keyNavigationDisabled = false;
+                        option.cb_after_nav(obj, gallIndex, thenext, theprev);
+                    });
+                }
+
+                /* -------- KEYBOARD HANDLER -------- */
+                function keyboardHandler(e) {
+                    if (e.keyCode === 27) { // esc
+                      closeVbox();
+                    }
+
+                    if (e.keyCode == 37 && prevok === true) { // left
+                      navigateGall(theprev);
+                    }
+
+                    if (e.keyCode == 39 && nextok === true) { // right
+                      navigateGall(thenext);
+                    }
+                }
+
+                /* -------- CLOSE VBOX -------- */
+                function closeVbox(){
+
+                    var cb_pre_close = option.cb_pre_close(obj, gallIndex, thenext, theprev);
+
+                    if (cb_pre_close === false) {
+                      return false;
+                    }
+
+                    $('body').off('keydown', keyboardHandler).removeClass('vbox-open');
+
+                    obj.focus();
+
+                    overlay.animate({opacity:0}, 500, function(){
+                      overlay.remove();
+                      keyNavigationDisabled = false;
+                      option.cb_post_close();
+                    });
+                }
+
+                /* -------- CLOSE CLICK -------- */
+                var closeclickclass = '.vbox-overlay';
+                if(!option.overlayClose){
+                    closeclickclass = '.vbox-close'; // close only on X
+                }
+
+                $('body').on('click touchstart', closeclickclass, function(e){
+                    if ($(e.target).is('.vbox-overlay') ||
+						$(e.target).is('.vbox-content') ||
+						$(e.target).is('.vbox-close') ||
+						$(e.target).is('.vbox-preloader') ||
+						$(e.target).is('.vbox-container')
+                    ) {
+                        closeVbox();
+                    }
+                });
+
+                startX = 0;
+                endX = 0;
+
+                diff = 0;
+                threshold = 50;
+                startouch = false;
+
+                function onDownEvent(e){
+                    content.addClass('vbox-animated');
+                    startY = endY = e.pageY;
+                    startX = endX = e.pageX;
+                    startouch = true;
+                }
+
+                function onMoveEvent(e){
+                    if (startouch === true) {
+                        endX = e.pageX;
+                        endY = e.pageY;
+
+                        diffX = endX - startX;
+                        diffY = endY - startY;
+
+                        var absdiffX = Math.abs(diffX);
+                        var absdiffY = Math.abs(diffY);
+
+                        if ((absdiffX > absdiffY) && (absdiffX <= 100)) {
+                            e.preventDefault();
+                            content.css('margin-left', diffX);
+                        }
+                    }
+                }
+
+                function onUpEvent(e){
+                    if (startouch === true) {
+                        startouch = false;
+                        var subject = obj;
+                        var change = false;
+                        diff = endX - startX;
+
+                        if (diff < 0 && nextok === true) {
+                            subject = thenext;
+                            change = true;
+                        }
+                        if (diff > 0 && prevok === true) {
+                            subject = theprev;
+                            change = true;
+                        }
+
+                        if (Math.abs(diff) >= threshold && change === true) {
+                            navigateGall(subject);
+                        } else {
+                            content.css({'margin-left': 0,'margin-right': 0});
+                        }
+                    }
+                }
+
+                /* == GLOBAL DECLERATIONS == */
+                var TouchMouseEvent = {
+                    DOWN: "touchmousedown",
+                    UP: "touchmouseup",
+                    MOVE: "touchmousemove"
+                };
+
+                /* == EVENT LISTENERS == */
+                var onMouseEvent = function(event) {
+                    var type;
+                    switch (event.type) {
+                        case "mousedown": type = TouchMouseEvent.DOWN; break;
+                        case "mouseup":   type = TouchMouseEvent.UP;   break;
+                        case "mouseout":   type = TouchMouseEvent.UP;   break;
+                        case "mousemove": type = TouchMouseEvent.MOVE; break;
+                        default:
+                            return;
+                    }
+                    var touchMouseEvent = normalizeEvent(type, event, event.pageX, event.pageY);
+                    $(event.target).trigger(touchMouseEvent);
+                };
+
+                var onTouchEvent = function(event) {
+                    var type;
+                    switch (event.type) {
+                        case "touchstart": type = TouchMouseEvent.DOWN; break;
+                        case "touchend":   type = TouchMouseEvent.UP;   break;
+                        case "touchmove":  type = TouchMouseEvent.MOVE; break;
+                        default:
+                            return;
+                    }
+
+                    var touch = event.originalEvent.touches[0];
+                    var touchMouseEvent;
+
+                    if (type == TouchMouseEvent.UP) {
+                         touchMouseEvent = normalizeEvent(type, event, null, null);
+                    } else {
+                        touchMouseEvent = normalizeEvent(type, event, touch.pageX, touch.pageY);
+                    }
+                    $(event.target).trigger(touchMouseEvent);
+                };
+
+                /* == NORMALIZE == */
+                var normalizeEvent = function(type, original, x, y) {
+                    return $.Event(type, {
+                        pageX: x,
+                        pageY: y,
+                        originalEvent: original
+                    });
+                };
+
+                /* == LISTEN TO ORIGINAL EVENT == */
+                if ("ontouchstart" in window) {
+                    $(document).on("touchstart", onTouchEvent);
+                    $(document).on("touchmove", onTouchEvent);
+                    $(document).on("touchend", onTouchEvent);
+                } else {
+                    $(document).on("mousedown", onMouseEvent);
+                    $(document).on("mouseup", onMouseEvent);
+                    $(document).on("mouseout", onMouseEvent);
+                    $(document).on("mousemove", onMouseEvent);
+                }
+
+                /* -------- LOAD AJAX -------- */
+                function loadAjax(){
+                  $.ajax({
+                  url: dest,
+                  cache: false
+                  }).done(function( msg ) {
+                      content.html('<div class="vbox-inline">'+ msg +'</div>');
+                      preloadFirst();
+
+                  }).fail(function() {
+                      content.html('<div class="vbox-inline"><p>Error retrieving contents, please retry</div>');
+                      updateoverlay();
+                  });
+                }
+
+                /* -------- LOAD IFRAME -------- */
+                function loadIframe(){
+                    content.html('<iframe class="venoframe" src="'+dest+'"></iframe>');
+                  //  $('.venoframe').load(function(){ // valid only for iFrames in same domain
+                    updateoverlay();
+                  //  });
+                }
+
+                /* -------- LOAD VIDEOs -------- */
+                function loadVid(autoplay){
+
+                    var player;
+                    var videoObj = parseVideo(dest);
+
+                    // set rel=0 to hide related videos at the end of YT + optional autoplay
+                    var stringAutoplay = autoplay ? "?rel=0&autoplay=1" : "?rel=0";
+                    var queryvars = stringAutoplay + getUrlParameter(dest);
+
+                    if (videoObj.type == 'vimeo') {
+                      player = 'https://player.vimeo.com/video/';
+                    } else if (videoObj.type == 'youtube') {
+                      player = 'https://www.youtube.com/embed/';
+                    }
+                    content.html('<iframe class="venoframe vbvid" webkitallowfullscreen mozallowfullscreen allowfullscreen allow="autoplay" frameborder="0" src="'+player+videoObj.id+queryvars+'"></iframe>');
+                    updateoverlay();
+                }
+
+                /**
+                * Parse Youtube or Vimeo videos and get host & ID
+                */
+                function parseVideo (url) {
+                    url.match(/(http:|https:|)\/\/(player.|www.)?(vimeo\.com|youtu(be\.com|\.be|be\.googleapis\.com))\/(video\/|embed\/|watch\?v=|v\/)?([A-Za-z0-9._%-]*)(\&\S+)?/);
+                    var type;
+                    if (RegExp.$3.indexOf('youtu') > -1) {
+                        type = 'youtube';
+                    } else if (RegExp.$3.indexOf('vimeo') > -1) {
+                        type = 'vimeo';
+                    }
+                    return {
+                        type: type,
+                        id: RegExp.$6
+                    };
+                }
+
+                /**
+                * get additional video url parameters
+                */
+                function getUrlParameter(name) {
+                  var result = '';
+                  var sPageURL = decodeURIComponent(name);
+                  var firstsplit = sPageURL.split('?');
+
+                  if (firstsplit[1] !== undefined) {
+                      var sURLVariables = firstsplit[1].split('&');
+                      var sParameterName;
+                      var i;
+                      for (i = 0; i < sURLVariables.length; i++) {
+                          sParameterName = sURLVariables[i].split('=');
+                          result = result + '&'+ sParameterName[0]+'='+ sParameterName[1];
+                      }
+                  }
+                  return encodeURI(result);
+                }
+
+                /* -------- LOAD INLINE -------- */
+                function loadInline(){
+                    content.html('<div class="vbox-inline">'+$(dest).html()+'</div>');
+                    updateoverlay();
+                }
+
+                /* -------- PRELOAD IMAGE -------- */
+                function preloadFirst(){
+                    images = content.find('img');
+
+                    if (images.length) {
+                        images.each(function(){
+                            $(this).one('load', function() {
+                                updateoverlay();
+                            });
+                        });
+                    } else {
+                        updateoverlay();
+                    }
+                }
+
+                /* -------- FADE-IN THE NEW CONTENT -------- */
+                function updateoverlay(){
+
+                    blocktitle.html(title);
+                    content.find(">:first-child").addClass('vbox-figlio').css({
+                        'width': framewidth,
+                        'height': frameheight,
+                        'padding': border,
+                        'background': bgcolor
+                    });
+
+                    $('img.vbox-figlio').on('dragstart', function(event) {
+                        event.preventDefault();
+                    });
+
+                    // reset content scroll
+                    container.scrollTop(0);
+
+                    updateOL();
+
+                    content.animate({
+                        'opacity': '1'
+                    },'slow', function(){
+                        $preloader.hide();
+                    });
+
+                    option.cb_content_loaded(obj, gallIndex, thenext, theprev);
+                }
+
+                /* -------- CENTER FRAME -------- */
+                function updateOL(){
+
+                    var sonH = content.outerHeight();
+                    var finH = $(window).height();
+
+                    if (sonH + 60 < finH) {
+                        margine = (finH - sonH)/2;
+                    } else {
+                        margine = '30px';
+                    }
+                    content.css('margin-top', margine);
+                    content.css('margin-bottom', margine);
+                    option.cb_post_resize();
+                }
+
+                $(window).resize(function(){
+                    if($('.vbox-content').length){
+                        setTimeout(updateOL(), 800);
+                    }
+                });
+            }); // each
+        } // venobox
+    }); // extend
+})(jQuery);
